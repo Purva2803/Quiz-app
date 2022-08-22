6fad36ebb922b332be4e71aa408342c95f618294
Purva Rajyaguru <>
2022-08-22 11:33:20 +0000

    adding a quiz app

---
+++ index.js
@@ -0,0 +1,39 @@
+var readlineSync = require('readline-sync');
+var answer = readlineSync.question('What is your name? :');
+console.log("welcome" + answer +" do you know purva? ");
+var answer1 = readlineSync.question("where do i live?");
+var score =0;                                   
+if (answer1==="Bhavnagar"){
+  console.log("right");
+  score +=1;
+}
+else{
+  console.log("wrong");
+  score -=1;
+}
+console.log("your score is" + score);
+  
+
+var answer2 = readlineSync.question("who is my favorite super would be?");
+if (answer2==="Batman"){
+  console.log("right");
+  score +=1;
+}
+else{
+  console.log("wrong");
+  score -=1;
+}
+console.log("your score is" + score);
+
+var answer2 = readlineSync.question("who is my favorite super would be?");
+if (answer2==="Batman"){
+  console.log("right");
+  score +=1;
+}
+else{
+  console.log("wrong");
+  score -=1;
+}
+console.log("your score is" + score);
+
+

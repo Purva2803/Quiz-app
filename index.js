5b1a5def8e5ee3df4ab40dba63465aae49c5efd6
Purva Rajyaguru <81159331+Purva2803@users.noreply.github.com>
2022-08-25 08:57:27 +0000

    I made a simple quiz.

--- index.js
+++ index.js
@@ -1,39 +1,49 @@
 var readlineSync = require('readline-sync');
 var answer = readlineSync.question('What is your name? :');
-console.log("welcome" + answer +" do you know purva? ");
+console.log("welcome" + answer + " do you know purva? ");
 var answer1 = readlineSync.question("where do i live?");
-var score =0;                                   
-if (answer1==="Bhavnagar"){
+var score = 0;
+if (answer1 === "Bhavnagar") {
   console.log("right");
-  score +=1;
+  score += 1;
 }
-else{
+else {
   console.log("wrong");
-  score -=1;
-}
-console.log("your score is" + score);
-  
-
-var answer2 = readlineSync.question("who is my favorite super would be?");
-if (answer2==="Batman"){
-  console.log("right");
-  score +=1;
-}
-else{
-  console.log("wrong");
-  score -=1;
+  score -= 1;
 }
 console.log("your score is" + score);
 
+
 var answer2 = readlineSync.question("who is my favorite super would be?");
-if (answer2==="Batman"){
+if (answer2 === "Batman") {
   console.log("right");
-  score +=1;
+  score += 1;
 }
-else{
+else {
   console.log("wrong");
-  score -=1;
+  score -= 1;
 }
 console.log("your score is" + score);
+
+
+var answer4 = readlineSync.question("where do i study?");
+if (answer4 === "Gec Bhavnagar") {
+  console.log("right"); NDJSN
+  score += 1;
+}
+else {
+  console.log("wrong");
+  score -= 1;
+}
+console.log("your score is" + score);
+console.log("YAY, your score is" + score);
+console.log('check out the score if you should  be there then ping me');
+console.log("highscores:");
+var highScores = [{
+  name: "Esha",
+  score: 3,
+}]
+
+highScores.map(score => console.log(score.name, " : ", score.score))
 
 
